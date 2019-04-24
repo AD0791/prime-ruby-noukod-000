0acef71490a8a8718c90f708e789b2557809c47a
Alexandro Disla <alexandrodisla@hotmail.com>
2019-04-24 03:25:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,21 @@
+=begin
 def prime?(num)
   return false if num < 0
   (2..(num - 1)).each do |n|
     return false if num % n == 0
   end
   true
+end
+=end
+
+def prime?(num)
+  if num < 0 
+    false
+  elsif num>0
+    (2..(num - 1)).each { |n|
+      return false if num % n == 0
+    }
+  else
+    true
+  end
 end
